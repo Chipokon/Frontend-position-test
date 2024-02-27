ecf9e4a3aea480e06e7c08c85e2f92a821af3d47
Dmitrii Koshmelev <100993336+Chipokon@users.noreply.github.com>
2024-02-28 01:47:39 +0300

    edit modal done

---
+++ src/views/EditModal/EditModal.tsx
@@ -0,0 +1,35 @@
+import { Button, Modal, TextField } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import styles from "./editModal.module.css";
+
+const EditModal = observer(({ selectedValue, onSave, onClose }) => {
+  const [newValue, setNewValue] = useState(selectedValue);
+  const handleInputChange = (event) => {
+    setNewValue(event.target.value);
+  };
+  const handleSave = () => {
+    onSave(newValue);
+    setNewValue('')
+  };
+  return (
+    <Modal
+      open={selectedValue !== null}
+      onClose={onClose}
+      className={styles.modal}
+    >
+      <div className={styles.blockInside}>
+        <TextField
+          defaultValue={selectedValue}
+          onChange={handleInputChange}
+          className={styles.input}
+        />
+        <Button onClick={handleSave} className={styles.button}>
+          Save
+        </Button>
+      </div>
+    </Modal>
+  );
+});
+
+export default EditModal;
--- src/views/TableComponent/TableComponent.tsx
+++ src/views/TableComponent/TableComponent.tsx
@@ -1,6 +1,8 @@
+
 import { observer } from "mobx-react-lite";
-import React from "react";
+import { useState } from "react";
 import {
+  Button,
   Paper,
   Table,
   TableBody,
@@ -12,8 +14,32 @@ import {
 import { useStores } from "../../customHooks/UseStore";
 import styles from "./tableComponent.module.css";
 import moment from "moment";
+import EditModal from "../EditModal/EditModal";
+
 const TableComponent = observer(() => {
   const store = useStores().TableStore;
+  const [selectedRowIndex, setSelectedRowIndex] = useState(null);
+  const [selectedKey, setSelectedKey] = useState(null);
+
+  const handleCellClick = (value, index, key) => {
+    setSelectedRowIndex(index);
+    setSelectedKey(key);
+    store.setSelectedValue(value);
+  };
+
+  const handleEditModalClose = () => {
+    setSelectedRowIndex(null);
+    setSelectedKey(null);
+    store.setSelectedValue(null);
+  };
+
+  const handleSaveValue = (newValue) => {
+    if (selectedRowIndex !== null && selectedKey) {
+      store.setNewValueInTable(selectedRowIndex, selectedKey, newValue);
+    }
+    handleEditModalClose();
+  };
+
   const getKeysNames = (obj) => {
     let keys = [];
     for (let key in obj) {
@@ -28,29 +54,35 @@ const TableComponent = observer(() => {
     return keys;
   };
 
-  const renderTableCells = (item) => {
-    return Object.values(item).map((value, index) => {
+  const renderTableCells = (item, index) => {
+    return Object.entries(item).map(([key, value], idx) => {
       if (typeof value === "object" && value !== null) {
-        return Object.keys(value).map((key, index) => (
-          <TableCell component="th" key={index}>
-            {value[key]}
+        return Object.keys(value).map((k, idx) => (
+          <TableCell component="th" key={idx}>
+            {value[k]}
           </TableCell>
         ));
       } else if (typeof value === "boolean") {
         return (
-          <TableCell component="th" key={index}>
+          <TableCell component="th" key={idx}>
             {value ? "Active" : "Not active"}
           </TableCell>
         );
       } else if (Date.parse(value)) {
         return (
-          <TableCell key={index}>
+          <TableCell component="th" key={idx}>
             {moment(value).format("DD-MM-YYYY hh:mm")}
+          </TableCell>
+        );
+      } else if (typeof value === "string") {
+        return (
+          <TableCell key={idx}>
+            <Button onClick={() => handleCellClick(value, index, key)}>{value}</Button>
           </TableCell>
         );
       } else {
         return (
-          <TableCell component="th" key={index}>
+          <TableCell component="th" key={idx}>
             {value}
           </TableCell>
         );
@@ -74,11 +106,16 @@ const TableComponent = observer(() => {
           </TableHead>
           <TableBody>
             {store.currentTableData.map((item, index) => (
-              <TableRow key={index}>{renderTableCells(item)}</TableRow>
+              <TableRow key={index}>{renderTableCells(item, index)}</TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <EditModal
+        selectedValue={store.selectedValue}
+        onSave={handleSaveValue}
+        onClose={handleEditModalClose}
+      />
     </div>
   );
 });
